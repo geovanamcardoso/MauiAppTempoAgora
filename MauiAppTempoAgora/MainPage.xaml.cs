4cae7cea1c70d21b160e901323fce73da257eed1
Geovana <geovanamscardoso183@gmail.com>
2026-04-04 21:19:34 -0300

    Criar classes, implementar busca, exibicao e tratamento de erros

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -1,25 +1,75 @@
-﻿namespace MauiAppTempoAgora
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+using System.Net;
+using System.Net.Http; 
+using System.Threading.Tasks;
+
+namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            try
+            {
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text.Trim());
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                    if(t != null)
+                    {
+                        string dados_previsao = " ";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                        dados_previsao = $"Cidade: {txt_cidade.Text}\n" +
+                            $"Latitude: {t.lat}\n" +
+                            $"Longitude: {t.lon}\n" +
+                            $"Temperatura: {t.temp_min}°C a {t.temp_max}°C\n" +
+                            $"Descrição: {t.main} - {t.description}\n" +
+                            $"Visibilidade: {t.visibility} metros\n" +
+                            $"Vento: {t.speed} m/s\n" +
+                            $"Nascer do Sol: {t.sunrise}\n" +
+                            $"Pôr do Sol: {t.sunset}";
+
+                        lbl_res.Text = dados_previsao;
+                    }
+                    else
+                    {
+                        lbl_res.Text = "Sem dados de previsão.";
+                    }
+                }
+                else
+                {
+                    lbl_res.Text = "Preencha o nome da cidade";
+                }
+            }
+            catch (HttpRequestException aex)
+            {
+                // Mensagem para cidade não encontrada
+                if (aex.StatusCode == HttpStatusCode.NotFound)
+                {
+                    await DisplayAlert("Cidade não encontrada", $"A cidade '{txt_cidade.Text}' não foi encontrada. Verifique o nome e tente novamente.", "OK");
+                    lbl_res.Text = string.Empty;
+                }
+                else if (aex.Message.Contains("Sem conexão"))
+                {
+                    // Alerta para sem conexão
+                    await DisplayAlert("Sem conexão", "Sem conexão com a internet. Verifique sua conexão e tente novamente.", "OK");
+                }
+                else
+                {
+                    await DisplayAlert("Erro", aex.Message, "OK");
+                }
+            }
+            catch(Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
         }
     }
-
 }
---
+++ MauiAppTempoAgora/Services/DataService.cs
@@ -0,0 +1,50 @@
+﻿using MauiAppTempoAgora.Models;
+using System.Text.Json.Nodes;
+
+namespace MauiAppTempoAgora.Services
+{
+    public class DataService
+    {
+        //É um método que pode retornar uma task nula (usamos '?'), caso o web service não responda
+        public static async Task<Tempo?> GetPrevisao(string cidade) 
+        {
+            Tempo? t = null;
+
+            string chave = "f91d9f685fcf5b8e5fbb12306b66bb46";
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={chave}&units=metric&lang=pt_br";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage resp = await client.GetAsync(url);
+
+                if(resp.IsSuccessStatusCode)
+                {
+                    string json = await resp.Content.ReadAsStringAsync();
+
+                    var rascunho = JsonObject.Parse(json);
+
+                    //Ambos valores estão em segundos (conta a partir de uma data da computacao), então precisamos converter para o formato de data e hora
+                    DateTime time = new();
+                    DateTime sunrise = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
+                    DateTime sunset = time.AddSeconds((double)rascunho["sys"]["sunset"]).ToLocalTime();
+
+                    t = new()
+                    {
+                        lat = (double)rascunho["coord"]["lat"],
+                        lon = (double)rascunho["coord"]["lon"],
+                        temp_min = (double)rascunho["main"]["temp_min"],
+                        temp_max = (double)rascunho["main"]["temp_max"],
+                        visibility = (int)rascunho["visibility"],
+                        sunrise = sunrise.ToString(),
+                        sunset = sunset.ToString(),
+                        speed = (double)rascunho["wind"]["speed"],
+                        description = (string)rascunho["weather"][0]["description"],
+                        main = (string)rascunho["weather"][0]["main"]
+                    }; //Fecha obj tempo
+                } //Fecha if de resposta bem sucedida
+            } ; //Fecha laço using
+
+            return t;
+        }
+    }
+}
